d6a00d0ceea3cd18cbe7fab448a9198ba7933e63
Kevin Lin <kevin.lin.p@gmail.com>
2024-07-27 20:24:27 -0700

    trying out maze

--- 0.4.0.py
+++ 0.4.0.py
@@ -1,108 +1,129 @@
-# TODO
-# [ ] keep track of harvestable pumpkins
-# [ ] target moving w/ distance/time estimation
-# [ ] task queue
-# [x] water multiple times
-# [x] batch buy
-# [x] pumpkin mode
+def run_0_4_0():
+	DEBUG = False
+	FERTILIZER_BATCH_BUY = 10
 
+	WIDTH = get_world_size()
+	HEIGHT = WIDTH
+	DIRECTIONS = [North, East, South, West]
 
-def run_0_4_1():
-    # constants
-    WIDTH = get_world_size()
-    HEIGHT = WIDTH
-    TANK_CAPACITY = 0.25
-    MAX_GROUND_WATER = 1.0
-    EMPTY_TANK_WOOD_COST = 5
+	def initialize_array(initial_value):
+		array = []
 
-	# parameters
-	target_hay = 3500
-	target_wood = 3500
-	target_carrots = 10000
-	grow_pumpkins = False
-	debug = False
-	limit_rows = 1
+		for _ in range(HEIGHT):
+			row = []
+			for _ in range(WIDTH):
+				row.append(initial_value)
+			array.append(row)
 
-	def move_next(x, y, grow_pumpkins):
-		move(East)
-		x = (x + 1) % WIDTH
-		if grow_pumpkins and x == 0:
-			move(South)
+		return array
+
+	def grow_hedges():
+		plant(Entities.Bush)
+
+		while get_entity_type() != Entities.Hedge:
+			if num_items(Entities.Fertilizer) == 0:
+				trade(Entities.Fertilizer, FERTILIZER_BATCH_BUY)
+			use_item(Items.Fertilizer)
+
+	def error():
+		while True:
+			do_a_flip()
+
+	def neighbor_position(position, direction):
+		x = position[0]
+		y = position[1]
+
+		if direction == North:
+			y = (y - 1) % HEIGHT
+		elif direction == East:
+			x = (x + 1) % WIDTH
+		elif direction == South:
 			y = (y + 1) % HEIGHT
+		elif direction == West:
+			x = (x - 1) % WIDTH
+		else:
+			error()
 		
 		return (x, y)
 
-	def plant_next(x, y):
-		water_if_needed()
+	def initialize_walls():
+		# [N, E, S, W]
+		walls = initialize_array([None, None, None, None])
 
-		if odd_tile(x, y) and num_items(Items.Wood) <= target_wood:
-			plant(Entities.Tree)
-		elif num_items(Items.Hay) <= target_hay:
-			plant(Entities.Grass)
-		elif num_items(Items.Carrot) <= target_carrots:
-			if num_items(Items.Carrot_Seed) == 0:
-				trade(Items.Carrot_Seed)
-			plant(Entities.Carrots)
+		for x in range(WIDTH):
+			walls[0][x][0] = True
+			walls[HEIGHT - 1][x][2] = True
+
+		for y in range(HEIGHT):
+			walls[y][0][3] = True
+			walls[y][WIDTH - 1][1] = True
+
+		return walls
+
+	def directions(previous_direction):
+		if previous_direction is None:
+			return DIRECTIONS
+		elif previous_direction == North:
+			return [West, North, East, South]
+		elif previous_direction == East:
+			return [North, East, South, West]
+		elif previous_direction == South:
+			return [East, South, West, North]
+		elif previous_direction == West:
+			return [South, West, North, East]
 		else:
-			if num_items(Items.Pumpkin_Seed) == 0:
-				trade(Items.Pumpkin_Seed)
-			plant(Entities.Pumpkin)
+			error()
 
-	def odd_tile(current_x, current_y):
-		return ((current_x % 2) + (current_y % 2)) == 1
+	def direction_index(direction):
+		if direction == North:
+			return 0
+		elif direction == East:
+			return 1
+		elif direction == South:
+			return 2
+		elif direction == West:
+			return 3
+		else:
+			error()
 
-	def waiting_for_harvest():
-		return get_entity_type() and not can_harvest()
+	def can_go(position, direction, walls):
+		current_wall = walls[position[1]][position[0]][direction_index(direction)]
+		if current_wall:
+			return False
 
-	def check_ground_type():
-		if get_ground_type() != Grounds.Soil:
-			till()
-		
-	def water_if_needed():
-		water_level = get_water()
-		
-		if debug:
-			quick_print(x, " ", y, " ", water_level)
-		
-		if water_level <= (MAX_GROUND_WATER - TANK_CAPACITY):
-			if num_items(Items.Water_Tank) > 0:
-				use_item(Items.Water_Tank)
-			else:
-				if num_items(Items.Wood) >= EMPTY_TANK_WOOD_COST:
-					trade(Items.Empty_Tank)
+		# TODO: set both on write instead
+		target_position = neighbor_position(position, direction)
+		opposite_wall = walls[target_position[1]][target_position[0]][(direction_index(direction) + 2) % 4]
 
-	def run():
-		x = get_pos_x()
-		y = get_pos_y()
-		ready_pumpkins = 0
+		if opposite_wall:
+			return False
+
+		return target_position
+
+	def go(current_position, previous_direction, walls):
+		for direction in directions(previous_direction):
+			target_position = can_go(current_position, direction, walls)
+
+			if target_position != False:
+				moved = move(direction)
+
+				if moved:
+					# not necessary ATM
+					# walls[current_position[1]][current_position[0]][direction_index(direction)] = False
+					go(target_position, direction, walls)
+				else:
+					walls[current_position[1]][current_position[0]][direction_index(direction)] = True
+
+	def solve_maze():
+		current_position = (get_pos_x(), get_pos_y())
+		# visited = initialize_array(False)
+		# visited[current_position[1]][current_position[0]] = True
+		walls = initialize_walls()
 
 		while True:
-			x, y = move_next(x, y, grow_pumpkins)
-			
-			check_ground_type()
-			entity = get_entity_type()
-			harvestable = can_harvest()
-			
-			if not entity:
-				plant_next(x, y)
-				continue
-			
-			if not harvestable:
-				water_if_needed()
-				continue
-
-			if grow_pumpkins and entity == Entities.Pumpkin:
-				ready_pumpkins = ready_pumpkins + 1
-				if ready_pumpkins == WIDTH * HEIGHT:
-					harvest()
-					plant_next(x, y)
-			else:
-				harvest()
-				plant_next(x, y)
-				
-			if x == 0 and y == 0:
-				ready_pumpkins = 0
+			grow_hedges()
+			go(current_position, None, walls)
 	
-	run()
+	solve_maze()
 
 run_0_4_0()
